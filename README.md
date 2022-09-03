2b95ade9a88dfd6f7f100d4376807395453e85a2
Matthieudegny <matthieu.dev.degny@gmail.com>
2022-09-03 11:57:38 +0200

    context API done

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ the isActive props inside NavLink component of react-router-dom
     <NavLink
         className={({ isActive }) => isActive ? activeLink : normalLink}>
     </NavLink>
+
+The useState hook returns a function to update the state. In our example, we know it as the setCounter(value) method. A specialty of this method is, you can get the previous(or old) state value to update the state. Please take a look into the code snippet below,
+
+    const incrBy3 = () => {
+    setCounter((prev) => prev + 3);
+    };
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,10 +5,11 @@ import { MdOutlineCancel } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 
 import { links } from '../data/dummy';
+import { useStateContext  } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
 
-  const activeMenu = true;
+  const { activeMenu, setActiveMenu } = useStateContext();
 
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
@@ -18,11 +19,17 @@ const Sidebar = () => {
       {activeMenu && (
         <>
         <div className="flex justify-between items-center">
-          <Link to="/" onClick={() => {}} className="items-center gap-3 ml-3 mt-4 flex text-x1 font-extrabold tracking-tight dark:text-white text-slate-900">
+          <Link 
+          to="/" 
+          onClick={() => {setActiveMenu(false)}} 
+          className="items-center gap-3 ml-3 mt-4 flex text-x1 font-extrabold tracking-tight dark:text-white text-slate-900">
             <SiShopware /> <span>Shoppy</span>
           </Link>
           <TooltipComponent content="Menu" position="BottomCenter">
-            <button type="button" onClick={() => {}} className="text-x1 rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden">
+            <button 
+            type="button" 
+            onClick={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} 
+            className="text-x1 rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden">
               <MdOutlineCancel />
             </button>
           </TooltipComponent>
