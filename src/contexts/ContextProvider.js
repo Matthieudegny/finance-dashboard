ae03674cc8a789ceb37a4c7c507844e5cb3eab7c
Matthieudegny <matthieu.dev.degny@gmail.com>
2022-09-06 20:10:46 +0200

    Navpage done

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -11,17 +11,28 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
 
-    const [activeMenu,setActiveMenu] = useState
+    const [activeMenu,setActiveMenu] = useState('');
+    const [isClicked, setIsClicked] = useState(initialState);
+    const [screenSize, setScreenSize] = useState(undefined)
+    const handleClick = (clicked) => {
+        setIsClicked({...initialState, [clicked]:true})
+    }
+
 
     return (
         <StateContext.Provider 
         value= {{ 
             activeMenu,
-            setActiveMenu 
+            setActiveMenu,
+            isClicked,
+            setIsClicked,
+            handleClick,
+            screenSize,
+            setScreenSize
         }}>
 
            { children }
-           
+
         </StateContext.Provider>
     )
 }
